01fc628aafdf50a7cc64720c7c1222d28c3aa5d4
Barbara Compagnoni <compagnb@newschool.edu>
2014-03-08 16:53:24 -0500

    Initial Creation

---
+++ HeartCharacterV2/HeartCharacterV2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HeartCharacterV2
+//
+//  Created by compagnb on 3/8/14.
+//  Copyright (c) 2014 compagnb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeartCharacterV2/HeartCharacterV2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HeartCharacterV2
+//
+
+//  Copyright (c) 2014 compagnb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end

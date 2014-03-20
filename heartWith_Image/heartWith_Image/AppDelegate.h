ac3f151167ee8d23adb421287bb94143097f6026
Barbara Compagnoni <compagnb@newschool.edu>
2014-03-20 16:36:30 -0400

    Code for class

---
+++ heartWith_Image/heartWith_Image/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  heartWith_Image
+//
+//  Created by compagnb on 3/20/14.
+//  Copyright (c) 2014 compagnb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

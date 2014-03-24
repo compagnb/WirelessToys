d291430666e7d9a15a12372e57811b7044856eac
Barbara Compagnoni <compagnb@newschool.edu>
2014-03-24 16:51:27 -0400

    Mid-Term

---
+++ PulseCharacter_nonSpriteKit_V3/PulseCharacter_nonSpriteKitTests/PulseCharacter_nonSpriteKitTests.m
@@ -0,0 +1,34 @@
+//
+//  PulseCharacter_nonSpriteKitTests.m
+//  PulseCharacter_nonSpriteKitTests
+//
+//  Created by compagnb on 3/23/14.
+//  Copyright (c) 2014 compagnb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PulseCharacter_nonSpriteKitTests : XCTestCase
+
+@end
+
+@implementation PulseCharacter_nonSpriteKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
